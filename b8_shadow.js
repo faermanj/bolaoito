29c63d1830dbc4b36d91c148f852f89b8481eeb7
Julio Faerman <jfaerman@gmail.com>
2015-12-01 11:26:40 -0200

    Shadow update

--- b8_shadow.js
+++ b8_shadow.js
@@ -5,29 +5,33 @@ var awsIot = require('aws-iot-device-sdk');
 
 var device_name = b8.cfg.clientId;
 
-
-function onConnect() {
-	console.log('Device connected ['+device_name+']');
-	ts.register( device_name );
+function fetchState(){
 	var token = ts.get(device_name);
 	console.log('Requested latest state ['+token+']')
 }
 
-function onDelta(thingName, shadow) {
-	console.log("Delta received: "+ JSON.stringify(state));
-	var state = shadow.state;
-	var l1 = state.l1;
-	var l2 = state.l2;
-	b8.tell_fortune(l1,l2);
-	b8_state = state;
+function onConnect() {
+	console.log('Device connected ['+device_name+']');
+	ts.register( device_name );
+	setTimeout(fetchState,2000);
 }
 
-function onStatus(thingName, stat, token, stateObject) {
-	console.log('received status '+stat+' on '+thingName+': '+JSON.stringify(stateObject));
-	var fn = cbs[token];
-	console.log('cb['+token+'] => ');
-	if(fn) fn(shadow);
-	cbs[token] = null;
+function onDelta(thingName, shadow) {
+	console.log("Delta received: "+ JSON.stringify(shadow));
+	onDesiredState(shadow.state);
+}
+
+function onDesiredState(state){
+	console.log("Updating desired state")
+	console.log(state);
+	b8.tell_fortune(state.l1,state.l2);
+}
+
+function onStatus(thingName, stat, token, shadow) {
+	console.log('received status '+stat+' on '+thingName+': '+JSON.stringify(shadow));
+	if ('accepted' == stat){
+		onDesiredState(shadow.state.desired);
+	}
 }
 
 function onTimeout(thingName, token) {
